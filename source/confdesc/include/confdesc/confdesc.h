45b64d24e9fe9f8a9db3c6d15ae65bfd50765d37
FalseCAM <falsecam@gmail.com>
2016-05-12 10:59:31 +0200

    CMake build structure

---
+++ source/confdesc/include/confdesc/confdesc.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+
+#include <string>
+
+#include <confdesc/confdesc_api.h>
+
+
+namespace cd
+{
+
+
+/**
+*  @brief
+*    Print information about the library to the console
+*/
+CONFDESC_API void printInfo();
+
+
+} // namespace baselib
---
+++ source/tests/confdesc-test/confdesc_test.cpp
@@ -0,0 +1,13 @@
+
+#include <gmock/gmock.h>
+
+
+class confdesc_test: public testing::Test
+{
+public:
+};
+
+TEST_F(confdesc_test, CheckSomeResults)
+{
+    // ...
+}
